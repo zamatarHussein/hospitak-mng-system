6bdf93b40ae00cb45bf83d0e6ae45a30ab3d0e61
zamatarHussein <125458906+zamatarHussein@users.noreply.github.com>
2023-03-25 07:28:25 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='HospitalManagementSystem',
+    version='2.7',
+    packages=['package'],
+    url='',
+    license='',
+    author='Tushar',
+    author_email='samatarhussein27@gmail.com',
+    description='Simple Hospital Management System Project'
+)
